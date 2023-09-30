187548bb466ff98e02a34fddd3068870adead5c8
Gustavo Albuquerque <gustavomachadoalbuquerque@gmail.com>
2023-09-29 22:08:29 -0300

    version 1.2

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -1,1 +1,32 @@
-document.querySelector(".initGameBtn").addEventListener("click",(function(){var e=document.querySelector(".inputName"),t=document.querySelector(".select");if(e.style.borderBottom="1px solid black",t.style.border="1px solid black",""===e.value)e.style.borderBottom="1px solid red",e.classList.add("error");else if(""===t.value)t.style.border="1px solid red";else{var l={playerName:e.value,theme:t.value};localStorage.setItem("play",JSON.stringify(l)),window.location.href="game.html"}}));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("var initGameBtn = function initGameBtn() {\n  var inputName = document.querySelector('.inputName');\n  var select = document.querySelector('.select');\n  inputName.style.borderBottom = '1px solid black';\n  select.style.border = '1px solid black';\n  if (inputName.value === '') {\n    inputName.style.borderBottom = '1px solid red';\n    inputName.classList.add('error');\n  } else if (select.value === '') {\n    select.style.border = '1px solid red';\n  } else {\n    var values = {\n      playerName: inputName.value,\n      theme: select.value\n    };\n    localStorage.setItem('play', JSON.stringify(values));\n    window.location.href = 'game.html';\n  }\n};\nvar btnInit = document.querySelector('.initGameBtn');\nbtnInit.addEventListener('click', initGameBtn);\n\n//# sourceURL=webpack://master-brain/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/index.js"]();
+/******/ 	
+/******/ })()
+;
